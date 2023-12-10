f08b5c950988ac21e3cba8e5af18864a37e74da4
calloc134 <115209243+calloc134@users.noreply.github.com>
2023-12-10 11:57:07 +0900

    fix: urql削除2

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./route";
-import { UrqlProvider } from "./lib/urql/UrqlProvider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <UrqlProvider>
-      <RouterProvider router={router} />
-    </UrqlProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
